26e22f7cae508cc2ae36a7ee0b1a8cfb628ef9d8
artyomthr <artyom.kudelich@yandex.by>
2024-02-23 03:16:38 +0300

    My first commit

---
+++ 3.py
@@ -0,0 +1,7 @@
+import sender_stand_request
+import data
+
+def get_user_body(first_name):
+    current_body = data.user_body.copy()
+    current_body["firstName"] = first_name
+    return current_body
